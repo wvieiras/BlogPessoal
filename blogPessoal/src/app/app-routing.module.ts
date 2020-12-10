f1da71281c0ac7deb97697c4b5c5d2954108ac6c
Wellington Vieira <wellington.vieira@fatec.sp.gov.br>
2020-12-10 19:04:46 -0300

    inclusão do put/delete postagens e put/delete temas

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { DeletePostagemComponent } from './delete-postagem/delete-postagem.component';
+import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
 import { FeedComponent } from './feed/feed.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PostTemaComponent } from './post-tema/post-tema.component';
+import { PutPostagemComponent } from './put-postagem/put-postagem.component';
+import { PutTemaComponent } from './put-tema/put-tema.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -12,6 +16,10 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
   {path: 'cadastro-tema', component: PostTemaComponent},
+  {path: 'editar-postagem/:id', component: PutPostagemComponent},
+  {path: 'deletar-postagem/:id', component: DeletePostagemComponent},
+  {path: 'editar-tema/:id', component: PutTemaComponent},
+  {path: 'deletar-tema/:id', component: DeleteTemaComponent},
   {path: '', redirectTo: 'home', pathMatch:'full'}
 ];
 
---
+++ blogPessoal/src/app/delete-tema/delete-tema.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
+
+@Component({
+  selector: 'app-delete-tema',
+  templateUrl: './delete-tema.component.html',
+  styleUrls: ['./delete-tema.component.css']
+})
+export class DeleteTemaComponent implements OnInit {
+
+  tema: Tema = new Tema()
+  
+  constructor(
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute,
+    //private alerta: AlertasService
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+    let id:number = this.route.snapshot.params["id"];
+    this.findByIdTema(id)
+  }
+
+  findByIdTema(id:number){
+    this.temaService.getByIdTema(id).subscribe((resp: Tema)=>{
+      this.tema = resp
+    });
+  } 
+
+  btnSim() {
+    if (this.tema.postagem.length != 0) {
+      alert('Esse tema não pode ser modificado, pois já pertence a uma postagem.')
+      this.router.navigate(['/cadastro-tema'])
+    } else {
+      this.temaService.deleteTema(this.tema.id).subscribe(() => {
+        this.router.navigate(['/cadastro-tema'])
+        alert('Tema apagado com sucesso!')
+      })
+    }
+
+  }
+
+  btnNao() {
+    this.router.navigate(['/cadastro-tema'])
+  }
+
+}
